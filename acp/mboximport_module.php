2c4c82e4ca46d27c3848a1bf6da5907fbfa28fe1
Thomas Pappas <getekid@gmail.com>
2017-04-26 16:17:00 +0200

    Added decode and analyse adapted to phpBB styling and functions
    Used example from test_message_decoder.php file in MIME Parse class

--- acp/mboximport_module.php
+++ acp/mboximport_module.php
@@ -55,8 +55,91 @@ class mboximport_module
 				trigger_error($lang->lang('ACP_MBOXIMPORT_MIME_PARSER_CLASS_NOT_FOUND') . adm_back_link($this->u_action));
 			}
 
-			// TODO Add check for Mbox file in the path
-			// TODO Add import function for Mbox file in the path
+			/*
+			 * Using the example from test_message_decoder.php,v 1.13 2012/04/11 09:28:19 mlemos Exp $
+			 */
+			$message_file = ($request->variable('mboximport_path', ''));
+			$mime = new mime_parser_class;
+
+			/*
+			 * Set to 0 for parsing a single message file
+			 * Set to 1 for parsing multiple messages in a single file in the mbox format
+			 */
+			$mime->mbox = 1;
+
+			/*
+			 * Set to 0 for not decoding the message bodies
+			 */
+			$mime->decode_bodies = 1;
+
+			/*
+			 * Set to 0 to make syntax errors make the decoding fail
+			 */
+			$mime->ignore_syntax_errors = 1;
+
+			/*
+			 * Set to 0 to avoid keeping track of the lines of the message data
+			 */
+			$mime->track_lines = 1;
+
+			/*
+			 * Set to 1 to make message parts be saved with original file names
+			 * when the SaveBody parameter is used.
+			 */
+			$mime->use_part_file_names = 0;
+
+			/*
+			 * Set this variable with entries that define MIME types not yet
+			 * recognized by the Analyze class function.
+			 */
+			$mime->custom_mime_types = array(
+				'application/vnd.openxmlformats-officedocument.wordprocessingml.document'=>array(
+					'Type' => 'ms-word',
+					'Description' => 'Word processing document in Microsoft Office OpenXML format'
+				)
+			);
+
+			$parameters=array(
+				'File'=>$message_file,
+				'SkipBody'=>0,
+			);
+
+			if (!$mime->Decode($parameters, $decoded))
+			{
+				$error_msg = $lang->lang('ACP_MBOXIMPORT_MIME_DECODING_ERROR') . ' ' . $mime->error . ' ' . $lang->lang('ACP_MBOXIMPORT_POSITION') . ' ' . $mime->error_position;
+				if ($mime->track_lines && $mime->GetPositionLine($mime->error_position, $line, $column))
+				{
+					$error_msg .= ' '. $lang->lang('ACP_MBOXIMPORT_LINE') . $line . ' ' . $lang->lang('ACP_MBOXIMPORT_COLUMN') . $column;
+				}
+				trigger_error($error_msg . adm_back_link($this->u_action));
+			}
+			else
+			{
+				for ($message = 0; $message < count($decoded); $message++)
+				{
+					if ($mime->decode_bodies)
+					{
+						if ($mime->Analyze($decoded[$message], $results))
+						{
+							// TODO Add import function for Mbox file in the path
+						}
+						else
+						{
+							trigger_error($lang->lang('ACP_MBOXIMPORT_MIME_ANALYSE_ERROR') . ' ' . $mime->error . adm_back_link($this->u_action));
+						}
+					}
+				}
+				for ($warning = 0, Reset($mime->warnings); $warning < count($mime->warnings); Next($mime->warnings), $warning++)
+				{
+					$w = Key($mime->warnings);
+					$error_msg = ($lang->lang('WARNING')) . ': ' . $mime->warnings[$w] . ' ' . $lang->lang('ACP_MBOXIMPORT_POSITION') . ' ' . $w;
+					if ($mime->track_lines && $mime->GetPositionLine($w, $line, $column))
+						$error_msg .= ' '. $lang->lang('ACP_MBOXIMPORT_LINE') . $line . ' ' . $lang->lang('ACP_MBOXIMPORT_COLUMN') . $column;
+					trigger_error($error_msg . adm_back_link($this->u_action));
+				}
+			}
+
+			// TODO Add message with the number of messages imported
 			trigger_error($lang->lang('ACP_MBOXIMPORT_IMPORT_SUCCESS') . adm_back_link($this->u_action));
 		}
 
--- language/en/info_acp_mboximport.php
+++ language/en/info_acp_mboximport.php
@@ -20,6 +20,13 @@ $lang = array_merge($lang, array(
 	'ACP_MBOXIMPORT_IMPORT'							=> 'Import',
 	'ACP_MBOXIMPORT_IMPORT_EXPLAIN'					=> 'Import an Mbox file',
 	'ACP_MBOXIMPORT_PATH'							=> 'File path',
+
+	// Messages
 	'ACP_MBOXIMPORT_MIME_PARSER_CLASS_NOT_FOUND'	=> 'MIME Parser class not found',
+	'ACP_MBOXIMPORT_MIME_DECODING_ERROR'			=> 'MIME message decoding error:',
+	'ACP_MBOXIMPORT_POSITION'						=> 'at position',
+	'ACP_MBOXIMPORT_LINE'							=> 'line',
+	'ACP_MBOXIMPORT_COLUMN'							=> 'column',
+	'ACP_MBOXIMPORT_MIME_ANALYSE_ERROR'				=> 'MIME message analyse error:',
 	'ACP_MBOXIMPORT_IMPORT_SUCCESS'					=> 'Mbox file has been imported successfully',
 ));
